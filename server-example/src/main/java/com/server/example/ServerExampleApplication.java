6d5de33be868133347815a6d9d720b8885a8b6a1
jinsheng <typistw@163.com>
2018-04-07 23:00:58 +0800

    testing sever

---
+++ server-example/src/main/java/com/server/example/ServerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.server.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerExampleApplication.class, args);
+	}
+}
